aee8bbee8ef134c30bf9e30e809dc0ea63e8574d
PolarisChen <polaris_chen@163.com>
2016-06-04 21:53:15 +0800

    Add app container

---
+++ app/js/containers/app.container.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Logo from '../components/logo.component';
+import Paper from '../components/paper.component';
+
+class AppContainer extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			itemValue: ''
+		}
+	}
+	handleChange(e) {
+		this.setState({itemValue: e.target.value});
+		if (e.keyCode == 13) {
+			alert("ssd")
+		}
+	}
+	render() {
+		let props = {};
+		props.handleChange = this.handleChange.bind(this);
+		props.itemValue = this.state.itemValue;
+		return (
+			<div className="app">
+      	<Logo />
+      	<Paper {...props} />
+      </div>
+		)
+	}
+}
+
+export default AppContainer;
