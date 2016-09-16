1e951590b148fdb4bfc586e22469cc6b5bc5bf0c
PolarisChen <polaris_chen@163.com>
2016-09-16 23:10:57 +0800

    Add stage-0 preset

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ module.exports = {
         loader: 'babel-loader',
         exclude: /node_modules/,
         query: {
-          presets: ['react', 'es2015']
+          presets: ['react', 'es2015', 'stage-0']
         }
       },
       { test: /\.css$/, loader: 'style-loader!css-loader' },
