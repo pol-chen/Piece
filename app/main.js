7397e705bc1e03754a9d0126ebf4ecfe80ba77d8
PolarisChen <polaris_chen@163.com>
2016-09-23 17:15:26 +0800

    Clean

--- app/main.js
+++ app/main.js
@@ -5,204 +5,10 @@ const Config = require('electron-config')
 
 const config = new Config()
 
-let appIcon = null
-let mainWindow = null
+let appIcon
+let mainWindow
 let contextMenu
-let mainMenu = Menu.buildFromTemplate([
-	{
-		label: "Piece",
-		submenu: [
-			{
-				label: "About Piece",
-				click: openAboutWindow
-			},
-			{
-				type: "separator"
-			},
-			{
-				label: 'Hide Piece',
-				accelerator: 'Command+H',
-				role: 'hide'
-			},
-			{
-				label: 'Hide Others',
-				accelerator: 'Command+Alt+H',
-				role: 'hideothers'
-			},
-			{
-				label: 'Show All',
-				role: 'unhide'
-			},
-			{
-				type: 'separator'
-			},
-			{
-				label: "Quit",
-				accelerator: "Command+Q",
-				click: function() {
-					app.quit()
-				}
-			}
-		]
-	},
-    {
-		label: "Edit",
-		submenu: [
-			{
-				label: "Undo",
-				accelerator: "CmdOrCtrl+Z",
-				selector: "undo:"
-			},
-			{
-				label: "Redo",
-				accelerator: "Shift+CmdOrCtrl+Z",
-				selector: "redo:"
-			},
-			{
-				type: "separator"
-			},
-			{
-				label: "Cut",
-				accelerator: "CmdOrCtrl+X",
-				selector: "cut:"
-			},
-			{
-				label: "Copy",
-				accelerator: "CmdOrCtrl+C",
-				selector: "copy:"
-			},
-			{
-				label: "Paste",
-				accelerator: "CmdOrCtrl+V",
-				selector: "paste:"
-			},
-			{
-				label: "Select All",
-				accelerator: "CmdOrCtrl+A",
-				selector: "selectAll:"
-			}
-		]
-	},
-	{
-		label: 'Window',
-		role: 'window',
-		submenu: [
-			{
-				label: 'Close',
-				accelerator: 'CmdOrCtrl+W',
-				role: 'close'
-			}
-		]
-	}
-])
-
-app.on('window-all-closed', () => {
-	if (process.platform != 'darwin') {
-		app.quit()
-	}
-})
-
-let aboutWindow = null
-function openAboutWindow() {
-	if (aboutWindow) {
-		return
-	}
-
-	aboutWindow = new BrowserWindow({
-		width: 366,
-		height: 434,
-		resizable: false,
-		minimizable: false,
-		maximizable: false,
-		fullscreenable: false,
-		alwaysOnTop: true
-	})
-
-	aboutWindow.loadURL('file://' + __dirname + '/about.html')
-
-	aboutWindow.on('resize', () => {
-		let size = aboutWindow.getSize()
-	})
-
-	aboutWindow.on('closed', () => {
-		aboutWindow = null
-	})
-}
-
-function installDevtools() {
-	const devtoolsInstaller = require('electron-devtools-installer')
-  devtoolsInstaller.default(devtoolsInstaller.REACT_DEVELOPER_TOOLS)
-    .then((name) => console.log(`Added Extension: ${name}`))
-    .catch((err) => console.log('An error occurred: ', err))
-}
-
-function openMainWindow() {
-	let frame = false
-	let alwaysOnTop = true
-
-	if (process.env.NODE_ENV === 'development') {
-		frame = true
-		alwaysOnTop = false
-	}
-
-	mainWindow = new BrowserWindow({
-		width: config.get('width'),
-		height: config.get('height'),
-		frame: frame,
-		alwaysOnTop: alwaysOnTop
-	})
-
-	if (config.has('x') && config.has('y')) {
-		mainWindow.setPosition(config.get('x'), config.get('y'))
-	}
-
-	mainWindow.loadURL('file://' + __dirname + '/index.html')
-
-	mainWindow.on('resize', () => {
-		let size = mainWindow.getSize()
-		config.set('width', size[0])
-		config.set('height', size[1])
-	})
-
-	mainWindow.on('move', () => {
-		let position = mainWindow.getPosition()
-		config.set('x', position[0])
-		config.set('y', position[1])
-	})
-
-	mainWindow.on('close', () => {
-		contextMenu.items[1].checked = false
-		appIcon.setContextMenu(contextMenu)
-	})
-
-	mainWindow.on('closed', () => {
-		mainWindow = null
-	})
-}
-
-
-
-function initConfig() {
-	if (!config.has('content')) {
-		const oldConfig = require('./config')
-		if (oldConfig.readConfig('x')) {
-			// Old config exists
-			config.set('x', oldConfig.readConfig('x'))
-			config.set('y', oldConfig.readConfig('y'))
-			config.set('width', oldConfig.readConfig('width'))
-			config.set('height', oldConfig.readConfig('height'))
-			config.set('content', oldConfig.readConfig('content'))
-		} else {
-			// Not exists
-			const defaultConfig = require('./config.default')
-			config.set('width', defaultConfig.width)
-			config.set('height', defaultConfig.height)
-			config.set('content', defaultConfig.content)
-		}
-		config.set('shortcutShow', 'Shift+Alt+S')
-		config.set('alwaysOnTop', true)
-	}
-}
+let aboutWindow
 
 app.on('ready', () => {
 	initConfig()
@@ -257,13 +63,116 @@ app.on('ready', () => {
 	} else {
 		app.dock.hide()
 	}
+})
 
-  Menu.setApplicationMenu(mainMenu)
+app.on('window-all-closed', () => {
+	if (process.platform != 'darwin') {
+		app.quit()
+	}
 })
 
 ipcMain.on('save-content', (event, arg) => {
 	config.set('content', arg)
 })
+
+function initConfig() {
+	if (!config.has('content')) {
+		const oldConfig = require('./config')
+		if (oldConfig.readConfig('x')) {
+			// Old config exists
+			config.set('x', oldConfig.readConfig('x'))
+			config.set('y', oldConfig.readConfig('y'))
+			config.set('width', oldConfig.readConfig('width'))
+			config.set('height', oldConfig.readConfig('height'))
+			config.set('content', oldConfig.readConfig('content'))
+		} else {
+			// Not exists
+			const defaultConfig = require('./config.default')
+			config.set('width', defaultConfig.width)
+			config.set('height', defaultConfig.height)
+			config.set('content', defaultConfig.content)
+		}
+		config.set('shortcutShow', 'Shift+Alt+S')
+		config.set('alwaysOnTop', true)
+	}
+}
+
+function openMainWindow() {
+	let frame = false
+	let alwaysOnTop = true
+
+	if (process.env.NODE_ENV === 'development') {
+		frame = true
+		alwaysOnTop = false
+	}
+
+	mainWindow = new BrowserWindow({
+		width: config.get('width'),
+		height: config.get('height'),
+		frame: frame,
+		alwaysOnTop: alwaysOnTop
+	})
+
+	if (config.has('x') && config.has('y')) {
+		mainWindow.setPosition(config.get('x'), config.get('y'))
+	}
+
+	mainWindow.loadURL('file://' + __dirname + '/index.html')
+
+	mainWindow.on('resize', () => {
+		let size = mainWindow.getSize()
+		config.set('width', size[0])
+		config.set('height', size[1])
+	})
+
+	mainWindow.on('move', () => {
+		let position = mainWindow.getPosition()
+		config.set('x', position[0])
+		config.set('y', position[1])
+	})
+
+	mainWindow.on('close', () => {
+		contextMenu.items[1].checked = false
+		appIcon.setContextMenu(contextMenu)
+	})
+
+	mainWindow.on('closed', () => {
+		mainWindow = null
+	})
+}
+
+function openAboutWindow() {
+	if (aboutWindow) {
+		return
+	}
+
+	aboutWindow = new BrowserWindow({
+		width: 366,
+		height: 434,
+		resizable: false,
+		minimizable: false,
+		maximizable: false,
+		fullscreenable: false,
+		alwaysOnTop: true
+	})
+
+	aboutWindow.loadURL('file://' + __dirname + '/about.html')
+
+	aboutWindow.on('resize', () => {
+		let size = aboutWindow.getSize()
+	})
+
+	aboutWindow.on('closed', () => {
+		aboutWindow = null
+	})
+}
+
+function installDevtools() {
+	const devtoolsInstaller = require('electron-devtools-installer')
+  devtoolsInstaller.default(devtoolsInstaller.REACT_DEVELOPER_TOOLS)
+    .then((name) => console.log(`Added Extension: ${name}`))
+    .catch((err) => console.log('An error occurred: ', err))
+}
 
 function toggleShow() {
 	if (!mainWindow) {
