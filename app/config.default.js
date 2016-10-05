68f934328514ff0cea8896d99a43c8c73afc4c44
PolarisChen <polaris_chen@163.com>
2016-10-05 11:17:46 +0800

    Update default config

--- app/config.default.js
+++ app/config.default.js
@@ -3,5 +3,5 @@
 module.exports = {
   "width": 435,
   "height": 623,
-  "content": "#Piece_1.1.0\n\nPiece is a simple temporary-note taking app. You could:\n\n- List what you gonna do recently\n\n- Or just write down your thoughts when inspiration hit you\n\n- Show/hide Piece with Alt+Shift+S @anywhere and @anytime\n\n- Highlight time, places, people and themes with @-handle and #-handle like this:\n\n  - Buy some fruits\n  - Update Piece #PROJECT @Tonight\n  - See the movie #StarTrek @Sep.22\n  - Finish the DM exercises #HOMEWORK @Fri.\n  - Design a landing page for Piece #PROJECT\n\nHope you like it :-D\n\nPolaris"
+  "content": "#Piece\n\nPiece is a simple #temporary-note taking app with which you could:\n\n- Write down your random #thoughts while you’re in the zone.\n- Manage your #to-do list in a simple way.\n- Show/hide Piece with Alt+Shift+S @anywhere and @anytime\n- Highlight time, places, people and themes with @-handle and #-handle like this:\n\n  - Buy some fruits\n  - Update Piece #PROJECT @Tonight\n  - See the movie #StarTrek @Sep.22\n  - Finish the DM exercises #HOMEWORK @Fri.\n  - Design a landing page for Piece #PROJECT\n\nPiece keeps your #temporary content in a single view, rather than a txt or doc.\n\nHope you like it :-D\n\nPolaris"
 }
